f12a72fb2c285401e3e6082b33307bf95edaeb13
Brandon Kidd <brandon.j.kidd@gmail.com>
2014-09-24 00:20:38 -0600

    Lean Components
    Setup npm and bower to load assets. Once loaded we use grunt to move
    only the files we need over into the theme. This makes for a clean
    portable theme while still using a task runner and package manager.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,39 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // copy files and folders form the components folder to the theme folder
+    // staying hip to package management while staying lean and portable
+    // documentation https://github.com/gruntjs/grunt-contrib-copy
+    copy: {
+      main: {
+        files: [
+          //BOOTSTRAP COMPONTENTS
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/bootstrap/less/*.less'], dest: 'theme/assets/less/bootstrap/', filter: 'isFile'},
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/bootstrap/less/mixins/*.less'], dest: 'theme/assets/less/bootstrap/mixins/', filter: 'isFile'},
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/bootstrap/js/*.js'], dest: 'theme/assets/js/bootstrap/', filter: 'isFile'},
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/bootstrap/fonts/**'], dest: 'theme/assets/fonts/bootstrap/', filter: 'isFile'},
+
+          // FONTAWESOME
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/font-awesome/less/*.less'], dest: 'theme/assets/less/fontawesome/', filter: 'isFile'},
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/font-awesome/fonts/**'], dest: 'theme/assets/fonts/fontawesome/', filter: 'isFile'},
+
+          // MODERNIZR JS
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/modernizr/modernizr`.js'], dest: 'theme/assets/js/modernizr/', filter: 'isFile'},
+
+          // RESPOND JS
+          // flattens results to a single level
+          {expand: true, flatten: true, src: ['components/respond/src/respond.js'], dest: 'theme/assets/js/respond/', filter: 'isFile'},
+        ]
+      }
+    },
+
     less: {
       development: {
         options: {
@@ -58,6 +91,7 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -65,6 +99,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.registerTask('migrate', ['copy']);
   grunt.registerTask('default', ['clean', 'less', 'jshint', 'uglify']);
 
 };
