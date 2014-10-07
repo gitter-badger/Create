9d9255cf024fc2db1293419a79b8a76cc945fca5
Joshua Kidd <kidd.josh.343@gmail.com>
2014-10-07 12:04:45 -0600

    Added some more custom fields stuff.

--- theme/functions/cf.php
+++ theme/functions/cf.php
@@ -9,20 +9,42 @@ class Fields {
     }
 
     function init() {
-        register_post_type('custom_field', array(
-            // 'labels' => $labels,
+        // Creates the post type
+        $labels = array(
+            'name' => __( 'Field&nbsp;Groups', 'cf' ),
+            'singular_name' => __( 'Custom Fields', 'cf' ),
+            'add_new' => __( 'Add New' , 'cf' ),
+            'add_new_item' => __( 'Add New Field Group' , 'cf' ),
+            'edit_item' =>  __( 'Edit Field Group' , 'cf' ),
+            'new_item' => __( 'New Field Group' , 'cf' ),
+            'view_item' => __('View Field Group', 'cf'),
+            'search_items' => __('Search Field Groups', 'cf'),
+            'not_found' =>  __('No Field Groups found', 'cf'),
+            'not_found_in_trash' => __('No Field Groups found in Trash', 'cf'), 
+        );
+
+        register_post_type('cf', array(
+            'labels' => $labels,
             'public' => false,
             'show_ui' => true,
             '_builtin' =>  false,
             'capability_type' => 'page',
             'hierarchical' => true,
             'rewrite' => false,
-            'query_var' => "custom_field",
+            'query_var' => "cf",
             'supports' => array(
                 'title',
             ),
-            'show_in_menu'  => true,
+            'show_in_menu'  => false,
         ));
+
+        if (is_admin()) {
+            add_action('admin_menu', array($this, 'admin_menu'));
+        }
+    }
+
+    function admin_menu() {
+        add_menu_page(__("Custom Fields", 'cf'), __("Custom Fields", 'cf'), 'manage_options', 'edit.php?post_type=cf', false, false, '80');
     }
 
 }
