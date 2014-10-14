8ddcc75c4fe0241b1fb7a45658c8f49541d62230
Joshua Kidd <kidd.josh.343@gmail.com>
2014-10-13 23:02:32 -0600

    Customizer Sections now working.

--- theme/functions/customizer/customizer.php
+++ theme/functions/customizer/customizer.php
@@ -1,53 +1,98 @@
 <?php
 
 if(!class_exists('CreateCustomizer')):
-class CreateCustomizer {
+endif;
 
-    public $path;
 
-    // Get everything setup.
-    function __construct($wp_customize) {
-        $this->path = get_template_directory() . '/functions/customizer/';
-        add_action('customize_register', array($this, 'register', $wp_customize), 30);
-    }
+function create_customize_register() {
+    global $wp_customize;
+    $path = get_template_directory() . '/functions/customizer/';
+    $section_path = $path . 'sections/';
 
-    public function register($wp_customize) {
-        require_once($this->path . 'sections/background.php');
-        add_action('customize_register', array($this, 'create_sections', $wp_customize), 30);
-    }
+    $sections = array(
+        'navbar' => array('title' => __('Navbar', 'create'), 'priority' => 35.10, 'path' => $section_path)
+    );
 
-    public function create_sections($wp_customize) {
-        $section_path = $this->path . 'sections/';
-        $sections = array(
-            'header' => array('title' => __('Header', 'create'), 'priority' => 35, 'path' => $section_path),
-            'navbar' => array('title' => __('Navbar', 'create'), 'priority' => 35.10, 'path' => $section_path),
-            'components' => array('title' => __('Components', 'create'), 'priority' => 35.12, 'path' => $section_path),
-            'colors'    => array('title' => __('Colors', 'create'), 'priority' => 35.14, 'path' => $section_path)
-        );
+    foreach ($sections as $section => $data) {
+        $file = trailingslashit($data['path']) . $section . '.php';
+        if(file_exists($file)) {
+            require_once($file);
+            $section_callback = 'create_customizer_' . $section;
+            if(function_exists($section_callback)) {
+                $section_id = 'create_' . $section;
 
-        foreach ($sections as $section => $data) {
-            $file = trailingslashit($data['path']) . $section . '.php';
-            if (file_exists($file)) {
-                require_once($file);
-                $section_cb = 'create_customizer_' . $section;
-                if(function_exists($section_cb)) {
-                    $section_id = 'create_customizer_' . esc_attr($section);
+                $wp_customize->add_section(
+                    $section_id,
+                    array(
+                        'title' => $data['title'],
+                        'priority' => $data['priority']
+                    )
+                );
 
-                    $wp_customize->add_section(
-                        $section_id,
-                        array(
-                            'title' => $data['title'],
-                            'priority' => $data['priority'],
-                        )
-                    );
-
-                }
+                call_user_func_array($section_callback, 
+                    array(
+                        $wp_customize,
+                        $section_id
+                    )
+                );
             }
         }
     }
-
 }
-endif;
 
-global $wp_customize;
-$customizer = new CreateCustomizer($wp_customize);
+add_action('customize_register', 'create_customize_register');
+
+// if(!class_exists('CreateCustomizer')):
+// class CreateCustomizer {
+
+//     public $path;
+
+//     // Get everything setup.
+//     function __construct($wp_customize) {
+//         $this->path = get_template_directory() . '/functions/customizer/';
+//         add_action('customize_register', array($this, 'register', $wp_customize), 30);
+//     }
+
+//     public function register($wp_customize) {
+//         require_once($this->path . 'sections/background.php');
+//         add_action('customize_register', array($this, 'create_sections', $wp_customize), 30);
+//     }
+
+//     public function create_sections($wp_customize) {
+//         $section_path = $this->path . 'sections/';
+//         $sections = array(
+//             'navbar' => array('title' => __('Navbar', 'create'), 'priority' => 35.10, 'path' => $section_path)
+//         );
+
+//         $wp_customize->add_section(
+//             'navbar',
+//             array(
+//                 'title' => 'Navbar',
+//                 'priority' => 30.50
+//             )
+//         );
+
+//         foreach ($sections as $section => $data) {
+//             $file = trailingslashit($data['path']) . $section . '.php';
+//             if (file_exists($file)) {
+//                 require_once($file);
+//                 $section_cb = 'create_customizer_navbar';
+//                 if(function_exists($section_cb)) {
+//                     $section_id = 'create_customizer_navbar';
+
+//                     $wp_customize->add_section(
+//                         $section_id,
+//                         array(
+//                             'title' => $data['title'],
+//                             'priority' => $data['priority'],
+//                         )
+//                     );
+//                 }
+//             }
+//         }
+//     }
+
+// }
+// endif;
+
+// $customizer = new CreateCustomizer($wp_customize);
--- theme/functions/customizer/sections/navbar.php
+++ theme/functions/customizer/sections/navbar.php
@@ -1,13 +1,24 @@
 <?php
 
 if(!function_exists('create_customizer_navbar')):
+function create_customizer_navbar($wp_customize, $section) {
+	// Basic Navbar color settings.
+	// Primary Link color.
+	$wp_customize->add_setting('navbar-link-primary',
+		array('default' => '#EEEEEE', 'type' => 'theme_mod')
+	);
 
-function create_customizer_navbar() {
-    global $wp_customize;
-    
-    $section = $wp_customize->get_section('navbar');
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'create_navbar-link-primary',
+			array(
+				'settings' => 'navbar-link-primary',
+				'section' => $section,
+				'label' => __('Primary Link Color', 'create'),
+				'priority' => 30.456
+			)
+		)
+	);
 }
-
 endif;
-
-add_action('customize_register', 'create_customizer_navbar');
