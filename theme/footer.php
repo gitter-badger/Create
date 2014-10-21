b09d59f13b50b0d30ed57cb882ef76b9cd0dd314
Joshua Kidd <kidd.josh.343@gmail.com>
2014-10-21 01:35:43 -0600

    Added extra footer stuff.

--- theme/footer.php
+++ theme/footer.php
@@ -11,14 +11,35 @@
 	</div>
 
 	<div class="container">
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="row">
+				<div class="col-md-6">
+					<?php
+					if(has_nav_menu('footer')):
+						wp_nav_menu(array('theme_location' => 'footer', 'menu_class' => 'footer-menu'));
+					endif;
+					?>
+				</div>
+				<div class="col-md-6">
+					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'create' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'create' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( __( 'Theme: %1$s by %2$s.', 'create' ), 'Create', '<a href="http://thecreate.com" rel="designer">Thecreate</a>' ); ?>
+				</div>
+			</div>
+		</footer>
+
+	</div>
+
+	<!-- <div class="container">
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'create' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'create' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
 				<?php printf( __( 'Theme: %1$s by %2$s.', 'create' ), 'Create', '<a href="http://thecreate.com" rel="designer">Thecreate</a>' ); ?>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div>
+			</div>
+		</footer>
+	</div> -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
